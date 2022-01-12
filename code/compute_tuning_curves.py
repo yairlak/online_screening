3b9afbb2398b1f08a999c3511d7b6d00c66fd0c8
Yair LAKRETZ <yair.lakretz@gmail.com>
2022-01-12 17:49:35 +0100

    refactorized and compatible with an encoding model for word-embeddings

--- code/compute_tuning_curves.py
+++ code/compute_tuning_curves.py
@@ -21,7 +21,16 @@ from viz import plot_weight_of_encoding_model
 from stats import add_FDR_correction
 
 parser = argparse.ArgumentParser()
-# GENERAL
+# SESSION/UNIT
+parser.add_argument('--session', default=None, type=str,
+                    help="If None, all sessions in folder are processed. \
+                        Otherwise, format should be '{subject}_{session}, \
+                            e.g., '90_1'.")
+parser.add_argument('--unit', default=None, type=int,
+                    help="Index to unit to process. \
+                        If None, all units will be processed.")
+
+# DATA AND MODEL
 parser.add_argument('--concept_source', default='Top-down Category (manual selection)',
                     help='Field name from THINGS for semantic categories',
                     choices = ['Bottom-up Category (Human Raters)',
@@ -61,25 +70,36 @@ data.load_metadata() # -> data.df_metadata
 data.load_neural_data() # -> data.neural_data
 data.load_word_embeddings() # -> data.df_word_embeddings
 
+# WHICH SESSION(S) TO RUN
+if args.session is None:
+    sessions = list(data.neural_data.keys())
+else:
+    sessions = [args.session]
+
+
 ############
 # ENCODING #
 ############
-list_dicts = []
-sessions = list(data.neural_data.keys())
+list_dicts = []    
 for session in sessions:
-    units = list(set(data.neural_data[session]['units'].keys()))
+    if args.unit is None:
+        units = list(set(data.neural_data[session]['units'].keys()))
+    else:
+        units = [args.unit]
     unit_names = [data.neural_data[session]['units'][unit]['channel_name'] for unit in units]
     
     for unit, unit_name in zip(units, unit_names):
-        print(f'{session}, Unit {unit}/({len(units)}), {unit_name} ({data.neural_data[session]["units"][unit]["kind"]})')
-        print('n_objects in "other"', len(data.neural_data[session]['dict_cat2object']['other']))
+        print(f'{session}, Unit {unit}/({len(units)}), {unit_name} \
+              ({data.neural_data[session]["units"][unit]["kind"]})')
+        print('Number of objects in "other" category:',
+              len(data.neural_data[session]['dict_cat2object']['other']))
         # PREPARE DATA FOR ENCODING MODEL
         X, X_names, remove = prepare_features(data, session, unit)
         y = prepare_neural_data(data, session, unit)
         
         # REMOVE trials with no feature-data (e.g., nan in word-embedding)
         if remove:
-            print('{len(remove)} trials were removed.')
+            print(f'{len(remove)} trials were removed.')
             X = np.delete(X, remove, axis=0)
             y = np.delete(y, remove, axis=0)
         
@@ -89,11 +109,12 @@ for session in sessions:
         
         # PLOT WEIGHTS OF ENCODING MODEL
         if not args.dont_plot:
-            fn_fig = f'weights_{args.feature_type}_{session}_{unit}_{unit_name}.png'
+            fn_fig = f'weights_{args.feature_type}_{session}_{unit_name}_{unit}.png'
             plot_weight_of_encoding_model(results, X_names,
                                           data,
-                                          session, unit,
+                                          session, unit, unit_name, 
                                           fn_fig, alpha=args.alpha)
+            print(f'Figure saved to: {fn_fig}')
         
         # COLLECT RESULTS INTO A DATAFRAME
         d = {'session':session,
@@ -111,9 +132,10 @@ for session in sessions:
         
 # SAVE RESULTS TO JSON
 df = pd.DataFrame(list_dicts)
-df = add_FDR_correction(df)         
-fn_df = '../results/encoding.json'
+# df = add_FDR_correction(df)         
+fn_df = f'../results/encoding_{args.session}_{args.unit}.json'
 df.to_json(fn_df)
+print(f'Results saved to {fn_df}')
 
     
     
--- code/viz.py
+++ code/viz.py
@@ -10,7 +10,7 @@ import os
 import matplotlib.pyplot as plt
 
 def plot_weight_of_encoding_model(results, X_names,
-                                  data, session, unit,
+                                  data, session, unit, unit_name,
                                   fn_fig, alpha=0.05):
     fig, ax = plt.subplots(figsize=(25, 10))
     if data.feature_type == 'semantic_categories':
@@ -30,7 +30,7 @@ def plot_weight_of_encoding_model(results, X_names,
     ax.set_ylabel('Coefficient size', fontsize=18)
     ax.set_ylim([0, 1])
     ax.set_xlabel('Semantic category', fontsize=24)
-    ax.set_title(f"{session}, Unit {unit}, {'unit name'} ({'kind'}); Model R2, R2-adjusted = {results['r2']:1.2f}, {results['r2_adj']:1.2f}", fontsize=20)
+    ax.set_title(f"{session}, Unit {'unit_name'}-{unit}, ({'kind'}); Model R2, R2-adjusted = {results['r2']:1.2f}, {results['r2_adj']:1.2f}", fontsize=20)
     plt.subplots_adjust(bottom=0.25)
     fig.savefig(os.path.join('../figures', fn_fig))
     plt.close(fig)
